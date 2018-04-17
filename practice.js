4141e03f31ae09cee86ee16655e6514827de3052
Steve <cooper.sk@gmail.com>
2018-04-17 15:07:57 -0600

    Update 2

--- practice.js
+++ practice.js
@@ -92,7 +92,11 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Code Here
 
 function evenFinder(nums){
-  for (var i = 0;;)
+  for (var i = 0; i<nums.length;i++){
+    if (nums[i]%2===0){
+      return nums.push([i]);
+    }
+  }
 }
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
